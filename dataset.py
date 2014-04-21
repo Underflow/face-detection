cdad82d3bede7ba04329f7018afe2254bdd69699
Quentin de Laroussilhe <q.delaroussilhe@underflow.fr>
2014-04-22 01:38:52 +0200

    Dataset loading

--- dataset.py
+++ dataset.py
@@ -17,24 +17,31 @@ def get_training_files():
             if extension == ".jpg":
                 res.append(os.path.join(root, file))
     return res
+
 def generate_dataset():
     tf = get_training_files()
-    dataset = []
+    dataset = np.empty([len(tf), 400], np.dtype("u1"))
     for i, image_file in enumerate(tf):
         progress("Loading pictures", int(float(i + 1) / len(tf) * 100))
         im = misc.imresize(misc.imread(image_file), (20, 20))
-        dataset.append(im)
+        for (x, y) in np.ndindex(20, 20):
+            dataset[i][x + y * 20] = np.dot(im[x][y], np.array([0.21, 0.71, 0.07]))
     f = open("dataset.bin", "w")
     pickle.dump(dataset, f)
     return dataset
 
-dataset = []
-if os.path.isfile("dataset.bin"):
-    print("Loading dataset.bin")
-    f = open("dataset.bin", "r")
-    dataset = pickle.load(f)
-else:
-    print("Generating dataset.bin")
-    dataset = generate_dataset()
+def load_dataset():
+    if os.path.isfile("dataset.bin"):
+        print("Loading dataset.bin")
+        f = open("dataset.bin", "r")
+        dataset = pickle.load(f)
+    else:
+        print("Generating dataset.bin")
+        dataset = generate_dataset()
 
+    return dataset
+
+dataset = load_dataset()
+for im in dataset:
+    print(im.shape)
 print("Let's play ?")
