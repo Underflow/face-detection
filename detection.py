85f026474bf177e0bc1a3a686cb66429da756643
Quentin de Laroussilhe <q.delaroussilhe@underflow.fr>
2015-03-14 01:15:58 +0100

    Theano refactor

--- detection.py
+++ detection.py
@@ -1,38 +1,41 @@
 import dataset
-import autoencoder
+import fullyconn
 import numpy as np
+import math
 from PIL import Image
 
 dataset = dataset.load_dataset()
 
-
-
 def train_autoencoder(dataset, batch_size, learning_rate, steps):
-    a = autoencoder.Autoencoder()
-    a.train(dataset[0:batch_size], 390, 0, 0.3)
+    nn = fullyconn.MLP(400)
+    nn.add_layer("bottleneck", 200)
+    nn.add_layer("output", 400)
+    train = nn.build_train(learning_rate)
 
     batch_count = len(dataset) / batch_size
     for _ in range(0, steps):
         for i in range(1, batch_count):
             ts = dataset[i * batch_size: (i + 1) * batch_size]
-            autoencoder.Neuralnet.train(a, ts, ts, steps, learning_rate)
+            print(math.sqrt(train(ts, ts) / 400))
 
-    return a
+    return nn
 
-batch_size = 40
-learning_rate = 0.5 / batch_size
+batch_size = 100
+learning_rate = 0.01
 steps = 1
 
 aa = train_autoencoder(dataset, batch_size, learning_rate, steps)
+eval = aa.build_eval()
 
-# Visualisation of the output layer of the autoencoder
 a = dataset[0]
-b = aa.eval(a)
+b = eval(np.array([a]))
 a = np.array(a.reshape(20, 20) * 255, dtype="uint8")
 b = np.array(b.reshape(20, 20) * 255, dtype="uint8")
-im = Image.fromarray(a)
+
+im  = Image.fromarray(a)
 im2 = Image.fromarray(b)
 imrgb = Image.merge("RGB", (im, im, im))
 imrgb2 = Image.merge("RGB", (im2, im2, im2))
-imrgb.show()
-imrgb2.show()
+
+imrgb.save("original.png")
+imrgb2.save("reconstructed.png")
---
+++ fullyconn.py
@@ -0,0 +1,99 @@
+import numpy as np
+import theano
+import theano.tensor as T
+from theano.tensor.shared_randomstreams import RandomStreams
+
+class FullyConnLayer():
+    def __init__(self,
+                 rng,
+                 name,
+                 dimension,
+                 input_dimension,
+                 theano_rng = None,
+                 input=None,
+                 W=None,
+                 b=None,
+                 activation=T.nnet.sigmoid):
+
+        self.activation = activation
+        self.dimension = dimension
+        self.input_dimension = input_dimension
+        if input is None:
+            self.input = T.dmatrix(name="input")
+        else:
+            self.input = input
+
+        if theano_rng is None:
+            theano_rng = RandomStreams(rng.randint(2 ** 30))
+
+        if W is None:
+            initial_W = np.asarray(
+                rng.uniform(
+                    low  = -4 * np.sqrt(6. / (input_dimension + dimension)),
+                    high = -4 * np.sqrt(6. / (input_dimension + dimension)),
+                    size = (input_dimension, dimension)
+                ),
+                dtype=theano.config.floatX
+            )
+            W = theano.shared(value=initial_W, name="W_" + name, borrow=True)
+
+        if b is None:
+            initial_b = np.zeros(
+                (dimension,),
+                dtype=theano.config.floatX
+            )
+            b = theano.shared(value=initial_b, name="b_" + name, borrow=True)
+
+        self.theano_rng = theano_rng
+        self.W = W
+        self.b = b
+        self.params = [self.W, self.b]
+        self.output = activation(T.dot(self.input, self.W) + self.b)
+
+class MLP():
+    def __init__(self, input_dimension, input=None):
+        self.rng = np.random.RandomState(42)
+        if input is None:
+            self.input = T.matrix("input")
+        else:
+            self.input = input
+        self.output = self.input
+        self.input_dimension = input_dimension
+        self.layers = []
+        self.params = []
+
+    def add_layer(self, name, dimension):
+        if self.layers == []:
+            self.layers.append(FullyConnLayer(self.rng,
+                                              name,
+                                              dimension,
+                                              self.input_dimension,
+                                              input=self.input))
+        else:
+            self.layers.append(FullyConnLayer(self.rng,
+                                              name,
+                                              dimension,
+                                              self.layers[-1].dimension,
+                                              input=self.layers[-1].output))
+        self.output = self.layers[-1].output
+        self.params = self.params + self.layers[-1].params
+
+    def build_train(self, learning_rate):
+        labels = T.matrix("labels", dtype=theano.config.floatX)
+        cost = T.sum((self.output - labels) ** 2)
+        gparams = [T.grad(cost, param) for param in self.params]
+        updates = [
+                (param, param - learning_rate * gparam) for param, gparam in zip(self.params, gparams)
+                ]
+
+        return theano.function(
+            inputs = [self.input, labels],
+            outputs=cost,
+            updates=updates
+        )
+
+    def build_eval(self):
+        return theano.function(
+            inputs = [self.input],
+            outputs = self.output
+        )
